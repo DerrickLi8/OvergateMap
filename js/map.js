0c76c19a351e47d9d89829831cead29fd95260f4
MarcGi <Marc.giannandrea@yahoo.co.uk>
2018-09-14 12:51:11 +0100

    Added map and controls onto homepage

---
+++ js/map.js
@@ -0,0 +1,7 @@
+var map = L.Wrld.map("map", "91579bb03b94dbe153485fb8b1033e8d", {
+    center: [56.460094, -2.972821],
+    zoom: 17,
+    indoorsEnabled: true
+  });
+
+  var indoorControl = new WrldIndoorControl("widget-container", map);
