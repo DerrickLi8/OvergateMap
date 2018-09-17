4e2e300cc795ebba5fda5a045014c402e8ad3283
MarcGi <Marc.giannandrea@yahoo.co.uk>
2018-09-17 02:21:04 +0100

    Added in popup content

--- js/map.js
+++ js/map.js
@@ -1,67 +1,140 @@
-  var map = L.Wrld.map("map", "91579bb03b94dbe153485fb8b1033e8d", {
-    center: [56.459801, -2.977928],
-    zoom: 15,
-    indoorsEnabled: true
-  });
 
-  var markers = [];
-  var markerController = new WrldMarkerController(map); 
-  var indoorControl = new WrldIndoorControl("widget-container", map);
-  var currentIndoorMapId;
-  var currentFloor;
+var map = L.Wrld.map("map", "91579bb03b94dbe153485fb8b1033e8d", {
+  center: [56.459801, -2.977928],
+  zoom: 15,
+  indoorsEnabled: true
+});
 
-  var poiApi = new WrldPoiApi("91579bb03b94dbe153485fb8b1033e8d");
+var markers = []
+var markerTitles = []
+var index = 0;
+var markerController = new WrldMarkerController(map);
+var indoorControl = new WrldIndoorControl("widget-container", map);
+var currentIndoorMapId;
+var currentFloor;
 
-  var lastMouseDown;
-  function onMouseDown(event) {
-    lastMouseDown = event.latlng;
-  }
-  
-  function onIndoorEntityClicked(event) {
-    event.ids.forEach(identifyEntity);
-  }
-  
-  function onIndoorMapEntered(event) {
-    currentIndoorMapId = event.indoorMap.getIndoorMapId();
-    currentFloor = map.indoors.getFloor().getFloorIndex();
-    poiApi.searchIndoors(currentIndoorMapId, currentFloor,onPOISearchResults,
-      {tags: "General"});
-  }
+var poiApi = new WrldPoiApi("91579bb03b94dbe153485fb8b1033e8d");
 
-  function onPOISearchResults(success, results){
-    if (success) 
-    { 
-      results.forEach(displayPOIMarker)
+function onIndoorMapEntered(event) {
+  currentIndoorMapId = event.indoorMap.getIndoorMapId();
+  currentFloor = map.indoors.getFloor().getFloorIndex();
+  poiApi.searchIndoors(currentIndoorMapId, currentFloor, onPOISearchResults,
+    { tags: "General" });
+}
+
+function onPOISearchResults(success, results) {
+  console.log(results);
+  if (success) {
+    var i;
+    for(i=0; i < results.length; i++){
+      (function () {
+        console.log(i + "-" + results.length);
+        var tempMarker = markerController.addMarker(i, [results[i].lat, results[i].lon], { iconKey: "toilet_men" });
+        markers.push(tempMarker);
+        var tempTitle = results[i].title
+        markerTitles.push(tempTitle);
+        var index = i;
+        tempMarker.on("click", function (e) { displayMarkerPopUp(index); });
+      }());
     }
   }
+}
 
-  function displayPOIMarker(marker){
-    console.log(marker);
-    var tempMarker = markerController.addMarker(markers,[marker.lat,marker.lon], {iconKey: "toilet_men"});
-    tempMarker.on("click", function(e) { displayMarkerPopUp(tempMarker); });
+function displayMarkerPopUp(markersIndex) {
+  marker=markers[markersIndex];
+  console.log(markersIndex);
+  title=markerTitles[markersIndex];
+  if (marker.getPopup()) {
+    console.log(marker.getPopup());
+    marker.unbindPopup();
   }
+  console.log(marker);
+  console.log(title);
+  var popupOptions = {
+    indoorMapId: currentIndoorMapId,
+    indoorMapFloorIndex: currentFloor,
+    autoClose: false,
+    closeOnClick: false,
+    minWidth: "5"
+  };
 
-  function onIndoorMapFloorChanged() {
-    currentFloor = map.indoors.getFloor().getFloorIndex();
-  }
-  
-  function identifyEntity(id) {
-    var latLng = lastMouseDown;
-    
-    var popupOptions = { 
-      indoorMapId: currentIndoorMapId, 
-      indoorMapFloorIndex: currentFloor, 
-      autoClose: false, 
-      closeOnClick: false,
-      minWidth: "5"          
-    };
-    var popup = L.popup(popupOptions)
-      .setLatLng(latLng)
-      .addTo(map)
-      .setContent(id);
-  }
+  var popup = L.popup(popupOptions)
+    .setLatLng(marker._latlng)
+    .addTo(map)
+    .setContent(
+      "<div class='panel panel-primary'>" +
+      "<div class='panel-heading'>" +
+      "<h2 class='panel-title'>" +
+      title + " Floor " + marker.options.indoorMapFloorIndex +
+      "</h2>" +
+      "</div>" +
+      "<div class='panel-body'>" +
+      "<div class='container'> " +
 
-  map.indoors.on("indoormapenter", onIndoorMapEntered);
-  map.indoors.on("indoormapfloorchange", onIndoorMapFloorChanged)
-  map.indoors.on("indoorentityclick", onIndoorEntityClicked);
-  map.on("mousedown", onMouseDown);
+      "<div id='myCarousel' class='carousel slide' data-ride='carousel'> " +
+      "		<script src ='js/carousel.js'></script>" +
+      "<br></br>" +
+      " <!-- Indicators --> " +
+      "<ol class='carousel-indicators' style='position:relative;background-color:rgba(0, 0, 0, 0.69);border-radius: 25px;padding-top:5px; border: 2px rgba(0, 0, 0, 0.69); margin-bottom:0px' > " +
+      " <li data-target='#myCarousel' data-slide-to='0' style='' class='active'></li> " +
+      "<li data-target='#myCarousel' data-slide-to='1' style=''></li> " +
+      "<li data-target='#myCarousel' data-slide-to='2' style=''></li> " +
+      "</ol> " +
+
+      " <!-- Wrapper for slides --> " +
+      " <div class='carousel-inner'> " +
+
+      "      <div class='item active'> " +
+      " <h3 class='ch3' style='text-align:center;color:darkgray'>People Density History</h3>" +
+      "        <img src='https://www.advsofteng.com/doc/cdnetdoc/images/colorbar.png' style='width:100%;' alt='...'>" +
+      "        <div class='carousel-caption'> " +
+      "       </div> " +
+      "      </div> " +
+
+      "      <div class='item'> " +
+      "        <img src='https://datavizcatalogue.com/methods/images/top_images/area_graph.png' style='width:100%;' alt='...'>" + 
+      "      <div class='carousel-caption'> " +
+      "   </div> " +
+      "</div> " +
+
+      "<div class='item'> " +
+      " <img src='https://www.charterworld.com/news/wp-content/uploads/2012/09/Graph-sqm.jpg' style='width:100%;' alt='...'>" +
+      "<div class='carousel-caption'> " +
+      "</div> " +
+      "</div> " +
+      "</div> " +
+      "</div>" +
+
+      "<div style='width:90%;margin-left:5%;'><hr align='center'></div>" +
+      "<h4 style='text-align:center;'>Current Statistics</h4>" +
+      "<div class='row' style='width:100%;'>" +
+      "<div class='col-md-6' style='text-align:center;'>"+
+      "<h4>"+ title + " Floor " + marker.options.indoorMapFloorIndex +"</h4>" +
+      "<div class='shopContent' style='text-align:center;'>"+
+        "<p>Current Visitors: 10" +
+        "<p>Current Population Density: 10/m2" +
+        "<p>Current Pop Density Rating: Average" +
+      "</div>"+
+      "</div>" +
+      "<div class='col-md-6' style='text-align:center;'>"+
+      "<h4>"+ title +"</h4>" +
+      "<div class='shopContent' style='text-align:center;'>"+
+        "<p>Current Visitors: 20" +
+        "<p>Current Population Density: 30/m2" +
+        "<p>Current Pop Density Rating: High" +
+      "</div>"+
+      "</div>" +
+      "</div>"+
+      "</div>"
+
+    );
+
+  marker.bindPopup(popup)
+}
+
+function onIndoorMapFloorChanged() {
+  currentFloor = map.indoors.getFloor().getFloorIndex();
+}
+
+map.indoors.on("indoormapenter", onIndoorMapEntered);
+map.indoors.on("indoormapfloorchange", onIndoorMapFloorChanged);
